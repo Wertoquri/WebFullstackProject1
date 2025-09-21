d6709564a43df1e68d0e1c87dd3d9cb0d2d9163f
wertoquri <kirilpojduk@gmail.com>
2025-09-21 15:33:20 +0300

    add ejs files

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let port = 3000;
 app.use(express.static('static'));
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('index', {products: ads});
 });
 
 app.post('/add', (req, res) => {
@@ -14,8 +17,10 @@ app.post('/add', (req, res) => {
     res.send({status: 'success' });
 });
 
-app.get('/ads', (req, res) => {
-    res.json(ads);
+app.use((req, res, next) => {
+    res.status(404);
+    res.render('notfound');
+    next();
 });
 
 app.listen(port, () => {
--- static/script.js
+++ static/script.js
@@ -1,18 +1,32 @@
-document.querySelector("form").addEventListener("submit", function(event) {
+document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
     fetch("/add", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ title: event.target["title"].value,
-                            desc: event.target["desc"].value })
-    }).then(()=> location.reload());
+        body: JSON.stringify({
+            title: event.target["title"].value,
+            desc: event.target["desc"].value
+        })
+    }).then(() => location.reload());
 });
 
-document.querySelector(".closeBtn").addEventListener("click", function() {
+document.querySelector(".closeBtn").addEventListener("click", function () {
     document.querySelector(".modal").style.display = "none";
 })
-document.querySelector("#openBtn").addEventListener("click", function() {
+document.querySelector("#openBtn").addEventListener("click", function () {
     document.querySelector(".modal").style.display = "grid";
 })
+
+// fetch("/ads")
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//         document.querySelector(".wrapper").innerHTML = data.map(el => `
+//             <div class="ads">
+//                 <h3>${el.title}</h3
+//                 <p>${el.desc}</p>
+//             </div>
+//         `).join("");
+// });
