5207955f9c9b4e28a65005b95ffd41a4f98e9c47
wertoquri <kirilpojduk@gmail.com>
2025-09-28 15:40:17 +0300

    add multer,uploads

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 let express = require('express');
+const multer = require('multer');
+const path = require('path');
+
 let ads = [];
 let app = express();
 let port = 3000;
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Appending extension
+    }
+});
+const upload = multer({storage});
 app.use(express.static('static'));
 app.use(express.json());
-
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -12,8 +27,11 @@ app.get('/', (req, res) => {
     res.render('index', {products: ads});
 });
 
-app.post('/add', (req, res) => {
-    ads.push(req.body);
+app.post('/add', upload.fields([{name:"image"}]), (req, res) => {
+    let data = req.body;
+    data.image = req.files.image.map((file) => file.filename);
+    data.id = ads.length;
+    ads.push(data);
     res.send({status: 'success' });
 });
 
